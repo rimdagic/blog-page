513998e1659a7b1ef09a011a8caf81c1604af687
Cheng/Dennis <115615421+thalassa111@users.noreply.github.com>
2024-01-12 19:56:17 +0100

    first commit

---
+++ server/src/main/java/com/example/matbloggen/MatbloggenApplication.java
@@ -0,0 +1,13 @@
+package com.example.matbloggen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatbloggenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatbloggenApplication.class, args);
+	}
+
+}
