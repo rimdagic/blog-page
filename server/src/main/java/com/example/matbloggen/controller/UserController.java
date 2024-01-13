32351f0c092c023bfde28b4a7540e94fd1a8a528
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2024-01-14 00:00:36 +0100

    jwt cookie in user controller

--- server/src/main/java/com/example/matbloggen/controller/UserController.java
+++ server/src/main/java/com/example/matbloggen/controller/UserController.java
@@ -2,13 +2,20 @@ package com.example.matbloggen.controller;
 
 import com.example.matbloggen.dtos.LoginRequestDto;
 import com.example.matbloggen.service.UserService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
+
+
 @RestController
+@CrossOrigin(origins = "*")
 public class UserController {
 
     private UserService userService;
@@ -19,7 +26,20 @@ public class UserController {
     }
 
     @PostMapping("/user/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestDto){
-        return ResponseEntity.ok(userService.generateTokenForUserByEmailAndPassword(loginRequestDto.email, loginRequestDto.password));
+    public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response){
+
+        String jwtToken =  userService.generateTokenForUserByEmailAndPassword(loginRequestDto.email, loginRequestDto.password);
+
+        Cookie cookie = new Cookie("jwtToken", jwtToken);
+        cookie.setMaxAge(36000); // Set the cookie expiration time in seconds (adjust as needed)
+        cookie.setPath("/"); // Set the cookie path
+
+        cookie.setSecure(true);
+        cookie.setHttpOnly(true);
+
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok("Login successful");
+
     }
 }
