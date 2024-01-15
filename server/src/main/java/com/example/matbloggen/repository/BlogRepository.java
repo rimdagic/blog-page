d96023dec1dcfeb3fe2f8292990ac3a4bf22f696
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2024-01-15 22:49:19 +0100

    added endpoints for blog posts

--- server/src/main/java/com/example/matbloggen/repository/BlogRepository.java
+++ server/src/main/java/com/example/matbloggen/repository/BlogRepository.java
@@ -2,11 +2,18 @@ package com.example.matbloggen.repository;
 
 import com.example.matbloggen.models.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface BlogRepository extends JpaRepository<Blog, UUID> {
 
+    List<Blog> findByHeadlineContaining(String searchWord);
+
+ //   @Query(value = "SELECT * FROM Blog", nativeQuery = true)
+ //   List<Blog> findBySearchWord(String searchWord);
 }
