6f66488bdd1b30590e96ff60fac262141a316f54
Admir Agic <112857246+rimdagic@users.noreply.github.com>
2024-01-18 18:00:43 +0100

    removed csrf from login, since user is not authenticated when trying to log in

--- client/js/login.js
+++ client/js/login.js
@@ -4,6 +4,8 @@ function submitForm() {
         password: document.getElementById('password').value
     };
 
+    var loginIsSuccessful = false;
+
     if (!email.checkValidity()) {
         alert('Fyll i en giltig email-adress.');
         return;
@@ -14,30 +16,36 @@ function submitForm() {
         return;
     }
 
+    console.log("Sending request")
+
+    //var csrfToken = getCsrfToken();
 
 
-    console.log("Fetching CSRF token");
 
-        fetch('http://localhost:8080/user/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            credentials: 'include', // Include credentials to ensure cookies are sent
-            body: JSON.stringify(formData),
-        })
-        .then(response => response.text())
+    fetch('http://localhost:8080/user/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+  //          'X-CSRF-TOKEN': csrfToken // Add this line to include the CSRF token
+        },
+        /* "credentials": "include", */
+        body: JSON.stringify(formData),
+    })
+        .then(response => response.text()) // Läs svaret som text
         .then(data => {
             console.log('Server Response:', data);
 
             if (data === "Login successful") {
-                window.location.href = "http://localhost:5500/home.html";
-            } else if (data === "Login successful admin") {
+               window.location.href = "http://localhost:5500/home.html";
+            }else if(data === "Login successful admin"){
                 window.location.href = "http://localhost:5500/admin-panel.html";
             }
         })
         .catch(error => {
             console.error('Error:', error);
         });
-
+}
+// Function to retrieve the CSRF token from the cookie or wherever it's stored
+function getCsrfToken() {
+    return document.cookie.replace(/(?:(?:^|.*;\s*)XSRF-TOKEN\s*=\s*([^;]*).*$)|^.*$/, '$1');
 }
